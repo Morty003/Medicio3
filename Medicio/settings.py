b6c9ba89539623d68fc517d1b0cb653010acbb3e
Artem <kotfsfs@gmail.com>
2022-07-21 16:34:03 +0300

    s

--- Medicio/settings.py
+++ Medicio/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'Medicio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql'
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
